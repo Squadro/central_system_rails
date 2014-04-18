2f40287db422b558131b3990b8e1652d62f80a5c
Nethra <lenovo@net.(none)>
2014-04-18 16:25:18 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PredictaWebapp::Application.config.secret_key_base = 'a88d3683c416b65de4046de02432ca426f4980c3d7b2974282d1e152ffba2a3888b58e8909505592ad7db65230ac9aa8d6b0f08198633ef268a392844e362c0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PredictaWebapp::Application.config.session_store :cookie_store, key: '_predicta_webapp_session'
